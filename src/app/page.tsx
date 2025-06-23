5785f8dfd1042dd3fc6d6ef8e1f67d9ea404206a
23navi <navisureka23@gmail.com>
2025-06-23 06:03:02 +0530

    add sidebar footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import {
   Sidebar,
+  SidebarContent,
+  SidebarFooter,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { ReactNode } from "react";
 
-export default function HomePage() {
+export default function HomePage({ children }: { children: ReactNode }) {
   return (
     <SidebarProvider className="overflow-y-hidden">
       <Sidebar collapsible="icon" className="overflow-hidden">
@@ -13,7 +19,16 @@ export default function HomePage() {
           <SidebarTrigger />
           <span className="text-xl text-nowrap">Sidebar</span>
         </SidebarHeader>
+        <SidebarContent></SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>My footer button</SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
       </Sidebar>
+      <main className="flex-1">{children}</main>
     </SidebarProvider>
   );
 }
